ca09534f857d2e6f77504fabc4b30c37a11c7989
limenVTech <limen@vt.edu>
2019-08-16 09:07:24 -0400

    Fix bug with Dissem. file counts

--- check_the_sums/check_sums_1.0.2.py
+++ check_the_sums/check_sums_1.0.2.py
@@ -49,7 +49,10 @@ def bag_sums(bagdir):
                     fname = member.name
                     if 'data' in fname:
                         bag_file_count += 1
-                    if 'DisseminationContent' or 'DisseminatedContent' in fname:
+                    if 'DisseminationContent' in fname:
+                        if not 'DisseminatedMetadata' in fname:
+                            dissem_actual += 1
+                    if 'DisseminatedContent' in fname:
                         if not 'DisseminatedMetadata' in fname:
                             dissem_actual += 1
                     if path.basename(fname) == 'manifest-md5.txt':
